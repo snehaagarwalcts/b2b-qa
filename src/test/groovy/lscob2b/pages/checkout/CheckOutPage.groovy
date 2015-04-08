2b95a4f32a18adbd59ffc3d180b2ae9c26ddfa34
9ammis8 <9ammis8@SFOLPK1YBXK.levi.com>
2015-04-07 20:36:39 -0700

    PerformanceTest

--- src/test/groovy/lscob2b/pages/checkout/CheckOutPage.groovy
+++ src/test/groovy/lscob2b/pages/checkout/CheckOutPage.groovy
@@ -44,7 +44,7 @@ class CheckOutPage extends Page{
 		waitFor { useAddressSelect.displayed }
 		useAddressSelect.click()
 		waitFor { placeOrderLink.displayed }
-		Thread.sleep(2000)
+		Thread.sleep(1000)
 		placeOrderLink.click()
 	}
 	
