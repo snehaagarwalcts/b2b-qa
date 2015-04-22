ed047e0809ca51b4565cc026b768a98be6c1bb0b
401685 <401685@PC210247.cts.com>
2015-04-22 17:16:02 +0530

    code update

--- src/test/groovy/lscob2b/pages/checkout/CheckOutPage.groovy
+++ src/test/groovy/lscob2b/pages/checkout/CheckOutPage.groovy
@@ -44,7 +44,7 @@ class CheckOutPage extends Page{
 		waitFor { useAddressSelect.displayed }
 		useAddressSelect.click()
 		waitFor { placeOrderLink.displayed }
-		Thread.sleep(2000)
+		Thread.sleep(3000)
 		placeOrderLink.click()
 	}
 	
